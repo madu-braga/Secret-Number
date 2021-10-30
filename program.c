b91b4059e6af5e1210b77456e736dd707c4d4dfe
Maria Eduarda Macedo Braga <78921099+madu-braga@users.noreply.github.com>
2021-10-30 09:04:30 -0300

    Create program.c

---
+++ program.c
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+int record=0;
+
+void limpa();       // Limpa a tela
+int gerar();        // Sorteia o número
+int palpite(int);   // Pega o palpite do usuário
+int checa(int, int);// Checa se errou ou acertou
+void dica(int, int);// Da a dica se o número é maior ou menor
+void encerra(int);  // Exibe as informações da partida
+int continuar();    // Opção de jogar novamente
+
+int main()
+{
+    int tentativas,
+        gerado,
+        palp;
+
+    do{
+        limpa();
+        tentativas=0;
+        cout<<"Record até o momento: "<< record <<" tentativas!"<<endl;
+        gerado = gerar();
+        cout<<"\nNúmero sorteado! "<<endl;
+
+        do{
+            tentativas++;
+            palp = palpite(tentativas);
+            dica(gerado, palp);
+        }while(checa(gerado, palp) != 1);
+
+        encerra(tentativas);
+    }while( continuar() );
+
+    return 0;
+}
